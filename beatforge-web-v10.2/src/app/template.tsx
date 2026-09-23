51eab21771fbc38cb6e5f48acc913ba55aef5395
valdemarbrandt5-alt <valdemarbrandt5@gmail.com>
2026-09-23 04:38:32 +0200

    Mount real ranked multiplayer client

---
+++ beatforge-web-v10.2/src/app/template.tsx
@@ -0,0 +1,6 @@
+'use client';
+import RankedMultiplayer from './RankedMultiplayer';
+
+export default function Template({children}:{children:React.ReactNode}){
+ return <><RankedMultiplayer/>{children}</>;
+}
