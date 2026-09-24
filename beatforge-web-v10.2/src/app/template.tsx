16ace79c08faec16a05215519b1a27171e1baf97
valdemarbrandt5-alt <valdemarbrandt5@gmail.com>
2026-09-24 04:09:42 +0200

    Load ranked bot queue and streak polish

--- beatforge-web-v10.2/src/app/template.tsx
+++ beatforge-web-v10.2/src/app/template.tsx
@@ -5,6 +5,7 @@ import {supabase} from '../lib/supabase';
 import '../ranked-ui-enhancements';
 import '../ranked-forfeit';
 import '../ranked-bots';
+import '../ranked-bot-ui';
 import '../main-ui-tweaks';
 import '../solo-ui-enhancements';
 import '../casual-social';
